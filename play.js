4dd74ee4ecaf528dca763abb9ab998e644392870
kunvar13 <kunvar13@gmail.com>
2021-11-03 16:08:18 -0600

    Sending name to the snake server

--- play.js
+++ play.js
@@ -1,19 +1,8 @@
-const net = require("net");
-const conn = net.createConnection({
-  host:'165.227.47.243', // IP address here,
-  port:50541// PORT number here,
-});
-// establishes a connection with the game server
-const connect = function() {
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  return conn;
-};
+const conn = require('./client');
 
 conn.on('data', (data) => {
   console.log('Message from client: ', data);
 });
 
 console.log("Connecting ...");
-connect();
+//conn();
