52488d7c65242160a49cb208ec3596904fb9c202
kunvar13 <kunvar13@gmail.com>
2021-11-04 14:09:23 -0600

    Attaching stdin

--- client.js
+++ client.js
@@ -1,7 +1,7 @@
 const net = require("net");
 const connect = function() {
   const conn = net.createConnection({
-    host:'165.227.47.243', // IP address here,
+    host:'172.16.1.71', // IP address here,
     port:50541// PORT number here,
   });
   // interpret incoming data as text
--- play.js
+++ play.js
@@ -5,4 +5,23 @@ conn.on('data', (data) => {
 });
 
 console.log("Connecting ...");
+
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  stdin.on('data', handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = function(stdin) {
+  if (stdin === '\u0003') {
+    process.exit();
+  }
+  // your code here
+};
 //conn();
